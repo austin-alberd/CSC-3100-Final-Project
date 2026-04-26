c886df87f5562aa74f5e7dfd665a6a4331000adf
Austin Alberd <austinalberd2005@gmail.com>
2026-04-25 22:39:31 -0500

    created event listeners for add, remove, and edit on the frontend

---
+++ frontend/js/script.js
@@ -0,0 +1,20 @@
+//Add Functions
+document.querySelectorAll(".add_button").forEach(button=>{
+    button.addEventListener("click",e=>{
+        const strButtonID=e.target.id
+    })
+})
+
+//Remove Functions
+document.querySelectorAll(".remove_button").forEach(button=>{
+    button.addEventListener("click",e=>{
+        const strButtonID=e.target.id
+    })
+})
+
+//Edit Functions
+document.querySelectorAll(".edit_button").forEach(button=>{
+    button.addEventListener("click",e=>{
+        const strButtonID=e.target.id
+    })
+})
