164cd4301bcc34622b87b18762a627c3a8068435
Austin Alberd <austinalberd2005@gmail.com>
2026-05-02 17:44:48 -0500

    AHHH

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -42,6 +42,8 @@ document.querySelectorAll(".add_button").forEach(button=>{
                         icon:"success",
                         text:"Added item successfully"
                     })
+
+                    document.querySelector(`#tbl${strTitle} tbody`).innerHTML+=`<tr id=${data.recordID}><td>${formValues[0]}</td><td>${formValues[1]}</td></tr>`
                 }
             })
         }else{
--- frontend/js/tables.js
+++ frontend/js/tables.js
@@ -1,3 +1,27 @@
-document.addEventListener("DOMContentLoaded",()=>{
-    
+document.addEventListener("DOMContentLoaded",async ()=>{
+    //Found the bellow on reddit. https://www.reddit.com/r/learnjavascript/comments/1dzrtwq/recommended_style_for_multiple_async_fetch_calls/
+    const [objExperience,objCredentails,objSkills] = await Promise.all([
+        (await fetch(`${strAPIBaseURL}/experience`)).json(),
+        (await fetch(`${strAPIBaseURL}/credentials`)).json(),
+        (await fetch(`${strAPIBaseURL}/skills`)).json()
+    ])
+
+    // Probably innefficient, but it works
+    const arrExperience = objExperience.rows
+    const arrCredentials = objCredentails.rows
+    const arrSkills = objSkills.rows
+    console.log(arrCredentials)
+
+    arrSkills.forEach(row=>{
+        document.querySelector("#tblSkill tbody").innerHTML += `<tr id=${row["skill_id"]}><td>${row["skill_name"]}</td><td>${row["skill_description"]}</td></tr>`
+    })
+
+    arrExperience.forEach(row=>{
+        document.querySelector("#tblExperience tbody").innerHTML += `<tr id=${row["experience_id"]}><td>${row["experience_name"]}</td><td>${row["experience_description"]}</td></tr>`
+    })
+
+
+    arrCredentials.forEach(row=>{
+        document.querySelector("#tblCredential tbody").innerHTML += `<tr id=${row["credential_id"]}><td>${row["credential_name"]}</td><td>${row["credential_description"]}</td></tr>`
+    })
 })
